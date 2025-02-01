5b303db961343ab62482abe4eed39ad226e5b86d
Kamil Cały <kamil.caly@vp.pl>
2025-02-01 17:56:40 +0100

    init

---
+++ ToDoList/Models/ToDoItem.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace ToDoList.Models
+{
+    public class ToDoItem
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; } = string.Empty;
+
+        [BsonElement("title")]
+        public string Title { get; set; } = string.Empty;
+
+        [BsonElement("description")]
+        public string Description { get; set; } = string.Empty;
+
+        [BsonElement("isCompleted")]
+        public bool IsCompleted { get; set; } = false;
+    }
+}
